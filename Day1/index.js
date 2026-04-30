b1ff428838b33e403f39eefc35cccf30f9143b27
Your Name <your@email.com>
2026-04-30 11:57:32 +0530

    Striing operations and Mathemetical operation

---
+++ Day1/index.js
@@ -0,0 +1,23 @@
+let a=5;
+let b=10;
+let str="Hello World";
+
+// string functions
+console.log(str);
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.indexOf("o"));
+
+//mahematical operations
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log(a-b);
+console.log(a%b);
+
+// logical operations
+console.log(a > b);
+console.log(a < b);
+console.log(a == b);
+console.log(a != b);
